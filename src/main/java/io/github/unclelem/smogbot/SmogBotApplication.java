7abd63bc055effd7bf612e3569e2c3afa9fbfcc3
uncle Lem <uncle.lem@yandex.ua>
2017-10-03 18:46:40 +0200

    Morning and evening configs separated

--- src/main/java/io/github/unclelem/smogbot/SmogBotApplication.java
+++ src/main/java/io/github/unclelem/smogbot/SmogBotApplication.java
@@ -3,16 +3,20 @@ package io.github.unclelem.smogbot;
 import twitter4j.TwitterException;
 
 import java.io.IOException;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Properties;
 
 public class SmogBotApplication {
+
+    private static final String ERROR_MESSAGE = "Something went wrong, please call the doctor";
+    private static final String TODAY = "morning.properties";
+    private static final String PROGNOSIS = "evening.properties";
+
     public static void main(String[] args) {
         Message message = null;
         try {
-            Properties properties = new Properties();
-            properties.load(SmogBotApplication.class.getClassLoader().getResourceAsStream("application.properties"));
-            message = new Retriever(properties).retrieve();
+            message = new Retriever(getConfigProperties()).retrieve();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -23,11 +27,22 @@ public class SmogBotApplication {
                 poster.post(message);
             } else {
                 poster.post(
-                        new Message("Something went wrong, please call the doctor", new ArrayList<>())
+                        new Message(ERROR_MESSAGE, new ArrayList<>())
                 );
             }
         } catch (TwitterException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Properties getConfigProperties() throws IOException {
+        Properties properties = new Properties();
+        LocalTime now = LocalTime.now();
+        if (now.getHour() < 12) {
+            properties.load(SmogBotApplication.class.getClassLoader().getResourceAsStream(TODAY));
+        } else {
+            properties.load(SmogBotApplication.class.getClassLoader().getResourceAsStream(PROGNOSIS));
+        }
+        return properties;
     }
 }
